03b338e928c6a8d6fbe935cf55083028c2995664
ari <ari@ariui-MacBook-Pro.local>
2020-02-15 09:35:30 +0900

    2주차 아토믹디자인

--- client/src/pages/Home/HomeMainContainer.tsx
+++ client/src/pages/Home/HomeMainContainer.tsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React from 'react';
+import { css } from '@emotion/core';
 
 interface Props {}
 
 const HomeMainContainer: React.FC<Props> = () => {
-    return <div>123</div>
-}
+  return (
+    <div>
+      <div
+        css={css`
+          height: 80px;
+          background-color: black;
+          color: white;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        `}
+      >
+        <div
+          css={css`
+            width: 100px;
+            height: 50px;
+            background-color: red;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-transform: uppercase;
+            font-size: 1.5rem;
+          `}
+        >
+          LOGO
+        </div>
+      </div>
+      <div
+        css={css`
+          height: 200px;
+          background-color: black;
+          color: white;
+        `}
+      >
+        SLIDER
+      </div>
+      <div
+        css={css`
+          height: calc(100vh - 80px - 200px - 80px);
+          background-color: black;
+          color: white;
+        `}
+      >
+        CONTENT
+      </div>
+      <div
+        css={css`
+          height: 40px;
+          background-color: black;
+          color: white;
+        `}
+      >
+        BANNER
+      </div>
+      <div
+        css={css`
+          height: 40px;
+          background-color: black;
+          color: white;
+        `}
+      >
+        FOOTER
+      </div>
+    </div>
+  );
+};
 
-export default HomeMainContainer
+export default HomeMainContainer;
---
+++ client/src/pages/Post/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+interface Props {}
+
+const Post: React.FC<Props> = () => {
+  return <div>안녕하세요</div>;
+};
+
+export default Post;
+//tsrafce  typescript react function export
